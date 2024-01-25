3d79771cdc000c2d430a6c36baae48ade333b04e
sekstini <sekstinilol@gmail.com>
2024-01-25 04:07:45 +0100

    Fix launch configs

--- configs/mamba_dac_44khz.sh
+++ configs/mamba_dac_44khz.sh
@@ -1,6 +1,6 @@
 python train.py \
-    --model_config "stable_audio_tools/configs/model_configs/txt2audio/mamba_dac_44khz.json" \
-    --dataset_config "stable_audio_tools/configs/dataset_configs/local_libritts.json" \
-    --save_dir "checkpoints/mamba/dac_44khz" \
-    --batch_size 8 \
-    --gradient_clip_val 0.2 \
+    --model-config "stable_audio_tools/configs/model_configs/txt2audio/mamba_dac_44khz.json" \
+    --dataset-config "stable_audio_tools/configs/dataset_configs/local_libritts.json" \
+    --save-dir "checkpoints/mamba/dac_44khz" \
+    --batch-size 8 \
+    --gradient-clip-val 0.2 \
