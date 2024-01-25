4e40602616232ac99f7388235391a27156b5e92d
sekstini <sekstinilol@gmail.com>
2024-01-25 04:07:45 +0100

    Fix launch configs

--- configs/mamba_encodec_32khz.sh
+++ configs/mamba_encodec_32khz.sh
@@ -1,6 +1,6 @@
 python train.py \
-    --model_config "stable_audio_tools/configs/model_configs/txt2audio/mamba.json" \
-    --dataset_config "stable_audio_tools/configs/dataset_configs/local_libritts.json" \
-    --save_dir "checkpoints/mamba/encodec_32khz" \
-    --batch_size 16 \
-    --gradient_clip_val 0.2 \
+    --model-config "stable_audio_tools/configs/model_configs/txt2audio/mamba.json" \
+    --dataset-config "stable_audio_tools/configs/dataset_configs/local_libritts.json" \
+    --save-dir "checkpoints/mamba/encodec_32khz" \
+    --batch-size 16 \
+    --gradient-clip-val 0.2 \
